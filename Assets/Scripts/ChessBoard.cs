4291c2b6cc25401787a4ae4503fc9c6069db1d34
Pablo Villaoslada Muñoz <pablovmlm@hotmail.com>
2025-07-23 16:52:21 +0200

    Board

--- Assets/Scripts/ChessBoard.cs
+++ Assets/Scripts/ChessBoard.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class ChessBoard : MonoBehaviour
 {
+    public GameObject whiteTile;
+    public GameObject blackTile;
+
+    public GameObject pawn;
+    public GameObject rook;
+    public GameObject knight;
+    public GameObject bishop;
+    public GameObject queen;
+    public GameObject king;
+
+
+    public int width = 8;
+    public int height = 8;
+    public float tileSize = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateGrid();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateGrid()
     {
-        
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                if ((i + j) % 2 == 0)
+                {
+                    Instantiate(blackTile, new Vector3(i * tileSize, j * tileSize, 0), Quaternion.identity, transform);
+                }
+                else
+                {
+                    Instantiate(whiteTile, new Vector3(i * tileSize, j * tileSize, 0), Quaternion.identity, transform);
+                }
+            }
+        }
+        float offsetX = (width - 1) * tileSize / 2f;
+        float offsetY = (height - 1) * tileSize / 2f;
+        transform.position = new Vector3(-offsetX, -offsetY, 0);
+    }
+    void GeneratePieces()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        SpriteRenderer sr = pawn.GetComponent<SpriteRenderer>();
+                        Instantiate(pawn, new Vector3(i * tileSize, j * tileSize, 0), Quaternion.identity, transform);
+                        break;
+
+                }
+            }
+        }
     }
 }
